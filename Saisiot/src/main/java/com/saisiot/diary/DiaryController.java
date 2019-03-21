778546fd64d2599729676e38628bc03ff93d7af7
ddoongi3152 <ddoongi3152@gmail.com>
2019-03-21 21:30:59 +0900

    last commit

--- Saisiot/src/main/java/com/saisiot/diary/DiaryController.java
+++ Saisiot/src/main/java/com/saisiot/diary/DiaryController.java
@@ -26,14 +26,13 @@ import com.saisiot.diary.biz.DiaryBiz;
 
 @Controller
 public class DiaryController {
-	
+
 	@Autowired
 	private DiaryBiz Dbiz;
 
 	@Autowired
 	private FileValidator fileValidator;
-	
-	
+
 	@RequestMapping(value = "/diary.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public String diary(Model model) {
 
@@ -48,107 +47,113 @@ public class DiaryController {
 		return "insert_diary";
 	}
 
-	
 	@RequestMapping(value = "/diary_insert.do", method = { RequestMethod.GET, RequestMethod.POST })
-	public String insert_Diary(@ModelAttribute DiaryDto dto, HttpServletRequest request, Model model, UploadFile uploadFile, BindingResult result)
-			throws IOException {
-		
+	public String insert_Diary(@ModelAttribute DiaryDto dto, HttpServletRequest request, Model model,
+			UploadFile uploadFile, BindingResult result) throws IOException {
 
-		// 유효성 검사
-		fileValidator.validate(uploadFile, result);
+		if (uploadFile.getFile().isEmpty() == false) {
+			// 유효성 검사
+			fileValidator.validate(uploadFile, result);
 
-		// 오류 검출
-		if (result.hasErrors()) {
-			return "insert_diary";
-		}
-
-		MultipartFile file = uploadFile.getFile();
-		String storefilename = file.getName();
-		String filename = file.getOriginalFilename();
-		System.out.println("���� �� ���� �� : " + storefilename);
-		System.out.println("�� �Ӿ� "+filename);
-
-		UploadFile fileobj = new UploadFile();
-		fileobj.setFilename(filename);
-
-		InputStream inputStream = null;
-		OutputStream outputStream = null;
-
-		try {
-			inputStream = file.getInputStream();
-			String path = WebUtils.getRealPath(request.getSession().getServletContext(), "upload/");
-
-			System.out.println("���ε� �� ���� ��� : " + path);
-
-			/*
-			 * ��� ������ : C:\workspace\....\storage ����� : ../(���� ����) ./(���� ����)
-			 * /(root->localhost:8787/ : �̵ڿ� �ٴ´�.)
-			*/ 
-
-			// storage�� �������� ������ �����.
-			File storage = new File(path);
-			if (!storage.exists()) {
-				storage.mkdirs();
+			// 오류 검출
+			if (result.hasErrors()) {
+				return "insert_diary";
 			}
 
-			// newfile�� �������� ������ newfile�� �����.
-			File newfile = new File(path + "/" + filename);
-			if (!newfile.exists()) {
-				newfile.createNewFile();
-			}
+			MultipartFile file = uploadFile.getFile();
+			String storefilename = file.getName();
+			String filename = file.getOriginalFilename();
+			System.out.println("���� �� ���� �� : " + storefilename);
+			System.out.println("�� �Ӿ� " + filename);
 
-			outputStream = new FileOutputStream(newfile);
+			UploadFile fileobj = new UploadFile();
+			fileobj.setFilename(filename);
 
-			int read = 0;
-			byte[] b = new byte[(int) file.getSize()];
+			InputStream inputStream = null;
+			OutputStream outputStream = null;
 
-			while ((read = inputStream.read(b)) != -1) {
-				outputStream.write(b, 0, read);
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
 			try {
-				inputStream.close();
-				outputStream.close();
+				inputStream = file.getInputStream();
+				String path = WebUtils.getRealPath(request.getSession().getServletContext(), "upload/");
+
+				System.out.println("���ε� �� ���� ��� : " + path);
+
+				/*
+				 * ��� ������ : C:\workspace\....\storage ����� : ../(���� ����) ./(���� ����)
+				 * /(root->localhost:8787/ : �̵ڿ� �ٴ´�.)
+				 */
+
+				// storage�� �������� ������ �����.
+				File storage = new File(path);
+				if (!storage.exists()) {
+					storage.mkdirs();
+				}
+
+				// newfile�� �������� ������ newfile�� �����.
+				File newfile = new File(path + "/" + filename);
+				if (!newfile.exists()) {
+					newfile.createNewFile();
+				}
+
+				outputStream = new FileOutputStream(newfile);
+
+				int read = 0;
+				byte[] b = new byte[(int) file.getSize()];
+
+				while ((read = inputStream.read(b)) != -1) {
+					outputStream.write(b, 0, read);
+				}
+
 			} catch (IOException e) {
 				e.printStackTrace();
+			} finally {
+				try {
+					inputStream.close();
+					outputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-		}
-		
-		String img_src = dto.getContent();
-		System.out.println("img : " + img_src);
-		
-		dto.setFileurl(filename);
-		dto.setPicurl(img_src);
-		
-		System.out.println("���� �Ϸοо�뷯��������"+dto.getContent());
-		System.out.println(dto.getMaplati());
-		System.out.println(dto.getMaplong());
-		System.out.println(dto.getTitle());
-		System.out.println(dto.getFileurl());
-		System.out.println(dto.getPicurl());
-		
-		int res = Dbiz.insert(dto);
-		
-		if(res >0) {
+
+			String img_src = dto.getContent();
+			System.out.println("img : " + img_src);
+
+			dto.setFileurl(filename);
+			dto.setPicurl(img_src);
+
+			System.out.println("���� �Ϸοо�뷯��������" + dto.getContent());
+			System.out.println(dto.getMaplati());
+			System.out.println(dto.getMaplong());
+			System.out.println(dto.getTitle());
+			System.out.println(dto.getFileurl());
+			System.out.println(dto.getPicurl());
+
+			int res = Dbiz.insert(dto);
+
+			if (res > 0) {
+
+				return "diary";
+
+			} else {
+
+				return "redirect:insert_diary";
+
+			}
+		} else {
+			
+			int res = Dbiz.insert(dto);
 			
 			return "diary";
-			
-		} else {
-				
-			return "redirect:insert_diary"; 
 			
 		}
 
 	}
-	
-	@RequestMapping(value="/diaryDetail.do", method= {RequestMethod.POST,RequestMethod.GET} )
-	public String diaryDetail(@RequestParam int diaryno ,Model model ) {
-		
+
+	@RequestMapping(value = "/diaryDetail.do", method = { RequestMethod.POST, RequestMethod.GET })
+	public String diaryDetail(@RequestParam int diaryno, Model model) {
+
 		model.addAttribute("dto", Dbiz.selectOne(diaryno));
-		
+
 		return "diary_detail";
 	}
 }
--- Saisiot/src/main/webapp/WEB-INF/views/diary_detail.jsp
+++ Saisiot/src/main/webapp/WEB-INF/views/diary_detail.jsp
@@ -1,10 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="resources/css/map.css"/>
 </head>
 <body>
 
@@ -23,18 +26,46 @@
 			<th>내	용</th>
 			<td>${dto.content }</td>
 		</tr>
-		<tr>
-			<td>
-			<input type="hidden" value="${dto.diaryno }"/>
-			<input type="hidden" value="${dto.folderno }"/>
-			<input type="file" value="${dto.fileurl }"/>
-			<input type="hidden" value="${dto.picurl }"/>
-			<input type="hidden" value="${dto.maplati }"/>
-			<input type="hidden" value="${dto.maplong }"/>
-			<input type="hidden" value="${dto.videourl }"/>
-			</td>
-		</tr>
 	</table>
+		<c:choose>
+			<c:when test="${ empty dto.maplati}">
+				<input type="hidden" value="${dto.maplati }" id="maplati"/>
+				<input type="hidden" value="${dto.maplong }" id="maplong"/>
+			</c:when>
+			<c:otherwise>
+				<div class="map_wrap">
+					<div id="map" style="width:100%;height:100%;position:relative;overflow:hidden;"></div>
+				</div>
+			</c:otherwise>
+		</c:choose>
+			<input type="hidden" value="${dto.diaryno }" name="diaryno"/>
+			<input type="hidden" value="${dto.folderno }" name="folderno"/>
+			<input type="file" value="${dto.fileurl }" name="fileurl"/>
+			<input type="hidden" value="${dto.picurl }" name="picurl"/>
+			<input type="hidden" value="${dto.maplati }" id="maplati"/>
+			<input type="hidden" value="${dto.maplong }" id="maplong"/>
+			<input type="hidden" value="${dto.videourl }" name="videourl"/>
+
+	
+	<script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=1fe75f64aaf4512f8f75ce29f8ceb483&libraries=services"></script>
+	<script>
+	// 마커를 담을 배열입니다
+	var markers = [];
+	
+	var maplati = $("#maplati").val();
+	var maplong = $("#maplong").val();
+	alert(maplati);
+	alert(maplong);
+
+	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+	    mapOption = {
+	        center: new daum.maps.LatLng(maplati, maplong), // 지도의 중심좌표
+	        level: 3 // 지도의 확대 레벨
+	    };  
+
+	// 지도를 생성합니다    
+	var map = new daum.maps.Map(mapContainer, mapOption);
+	</script>
 
 </body>
 </html>
