e418e7b5df2ac7b716ce0e492ff56836508b322a
Choi-Seung-Eon <chldjs@naver.com>
2019-03-26 17:23:01 +0900

    ^^

--- Saisiot/src/main/java/com/saisiot/userinfo/biz/UserinfoBizImpl.java
+++ Saisiot/src/main/java/com/saisiot/userinfo/biz/UserinfoBizImpl.java
@@ -169,6 +169,24 @@ public class UserinfoBizImpl implements UserinfoBiz {
 	}
 	//seo's editing end----------
 
+	@Override
+	public int comebackuser(UserinfoDto dto) {
+		// TODO Auto-generated method stub
+		return dao.comebackuser(dto);
+	}
+
+	@Override
+	public int snscomback(UserinfoDto dto) {
+		// TODO Auto-generated method stub
+		return dao.snscomback(dto);
+	}
+
+	@Override
+	public int userinfoplus(UserinfoDto dto) {
+		// TODO Auto-generated method stub
+		return dao.userinfoplus(dto);
+	}
+
 
 	
 	
