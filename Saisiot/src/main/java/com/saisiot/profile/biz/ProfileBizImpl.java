b9422bd07dc2a38e50aa29afce6b0fbdd7cea151
NojoYuJung <wwwwwwwwwwwasdsa@gmail.com>
2019-03-26 19:19:51 +0900

    profile(error)

--- Saisiot/src/main/java/com/saisiot/profile/biz/ProfileBizImpl.java
+++ Saisiot/src/main/java/com/saisiot/profile/biz/ProfileBizImpl.java
@@ -3,6 +3,7 @@ package com.saisiot.profile.biz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.saisiot.profile.dao.ProfileDao;
+import com.saisiot.profile.dao.ProfileDaoImpl;
 import com.saisiot.profile.dto.ProfileDto;
 
 @Service
@@ -13,6 +14,14 @@ public class ProfileBizImpl implements ProfileBiz{
 	
 	@Override
 	public int insert_P(String email) {
+		
+		dao = new ProfileDaoImpl();
+		
+		if(dao == null) {
+			System.out.println("널이다!");
+		}else {
+			System.out.println(">>>>>>>>>>>"+dao.insert_P(email));
+		}
 		
 		return dao.insert_P(email);
 	}
--- Saisiot/src/main/java/com/saisiot/profile/dao/ProfileDaoImpl.java
+++ Saisiot/src/main/java/com/saisiot/profile/dao/ProfileDaoImpl.java
@@ -12,10 +12,17 @@ public class ProfileDaoImpl implements ProfileDao{
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
+	public ProfileDaoImpl() {
+		System.out.println("dao객체 생성");
+	}
+	
 	@Override
 	public int insert_P(String email) {
 		int res = 0;
-		
+		if(sqlSession==null) {
+			System.out.println("null break");
+			return 0;
+		}
 		res = sqlSession.insert(NAMESPACE + "insert_p", email);
 		
 		return res;
